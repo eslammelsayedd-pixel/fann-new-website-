f1f416d1db7e73f32c8dafddbb464fe50d5aa971
eslammelsayedd-pixel <eslammelsayedd@gmail.com>
2026-03-26 15:00:34 +0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
@@ -7,7 +6,7 @@ import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import ServicesPage from './pages/ServicesPage';
 import PortfolioPage from './pages/PortfolioPage';
-import ProjectDetailPage from './pages/ProjectDetailPage'; // Import
+import ProjectDetailsPage from './pages/ProjectDetailsPage'; // Import
 import AIDesignStudioPage from './pages/AIDesignStudioPage';
 import ExhibitionStudioPage from './pages/ExhibitionStudioPage';
 import DesignResultPage from './pages/DesignResultPage';
@@ -31,53 +30,52 @@ import TestimonialsPage from './pages/TestimonialsPage';
 // New Lead Magnet Pages
 import ExhibitionGuidePage from './pages/resources/ExhibitionGuidePage';
 import TrendsReportPage from './pages/resources/TrendsReportPage';
-import CostCalculatorPage from './pages/resources/CostCalculatorPage';\nimport CostGuidePage from './pages/resources/CostGuidePage';
+import CostCalculatorPage from './pages/resources/CostCalculatorPage';
+import CostGuidePage from './pages/resources/CostGuidePage';
 import ConsultationPage from './pages/ConsultationPage';
 
 const App: React.FC = () => {
-    const location = useLocation();
+  const location = useLocation();
 
-    return (
-        <ApiKeyProvider>
-            <Layout>
-                <AnimatePresence mode="wait">
-                    <Routes location={location} key={location.pathname}>
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="/services" element={<ServicesPage />} />
-                        {/* Service Detail Pages */}
-                        <Route path="/services/custom-exhibition-stands-dubai" element={<CustomStandsPage />} />
-                        <Route path="/services/modular-exhibition-systems-dubai" element={<ModularSystemsPage />} />
-                        <Route path="/services/turnkey-exhibition-services-uae" element={<TurnkeyServicesPage />} />
-                        <Route path="/services/exhibition-stand-fabrication-dubai" element={<FabricationPage />} />
-                        <Route path="/services/interior-fitout-exhibition-spaces-dubai" element={<FitOutPage />} />
-                        
-                        <Route path="/portfolio" element={<PortfolioPage />} />
-                        <Route path="/portfolio/:slug" element={<ProjectDetailPage />} /> {/* New Route */}
-                        
-                        <Route path="/testimonials" element={<TestimonialsPage />} />
-                        <Route path="/fann-studio" element={<AIDesignStudioPage />} />
-                        <Route path="/fann-studio/exhibition" element={<ExhibitionStudioPage />} />
-                        <Route path="/fann-studio/exhibition/result" element={<DesignResultPage />} />
-                        <Route path="/fann-studio/event" element={<EventStudioPage />} />
-                        <Route path="/fann-studio/event/result" element={<EventResultPage />} />
-                        <Route path="/fann-studio/interior" element={<InteriorStudioPage />} />
-                        <Route path="/fann-studio/interior/result" element={<InteriorResultPage />} />
-                        <Route path="/events-calendar" element={<EventsCalendarPage />} />
-                        <Route path="/about" element={<AboutPage />} />
-                        <Route path="/contact" element={<ContactPage />} />
-                        <Route path="/insights" element={<InsightsPage />} />
-                        <Route path="/roi-calculator" element={<ROICalculatorPage />} />
-                        
-                        {/* Resources & Lead Magnets */}
-                        <Route path="/resources/exhibition-guide" element={<ExhibitionGuidePage />} />
-                        <Route path="/resources/trends-2026" element={<TrendsReportPage />} />
-                        <Route path="/resources/cost-calculator" element={<CostCalculatorPage />} />\n                        <Route path="/resources/exhibition-stand-cost-dubai" element={<CostGuidePage />} />
-                        <Route path="/book-consultation" element={<ConsultationPage />} />
-                    </Routes>
-                </AnimatePresence>
-            </Layout>
-        </ApiKeyProvider>
-    );
+  return (
+    <ApiKeyProvider>
+      <Layout>
+        <AnimatePresence mode=\"wait\">
+          <Routes location={location} key={location.pathname}>
+            <Route path=\"/\" element={<HomePage />} />
+            <Route path=\"/services\" element={<ServicesPage />} />
+            {/* Service Detail Pages */}
+            <Route path=\"/services/custom-exhibition-stands-dubai\" element={<CustomStandsPage />} />
+            <Route path=\"/services/modular-exhibition-systems-dubai\" element={<ModularSystemsPage />} />
+            <Route path=\"/services/turnkey-exhibition-services-uae\" element={<TurnkeyServicesPage />} />
+            <Route path=\"/services/exhibition-stand-fabrication-dubai\" element={<FabricationPage />} />
+            <Route path=\"/services/interior-fitout-exhibition-spaces-dubai\" element={<FitOutPage />} />
+            <Route path=\"/portfolio\" element={<PortfolioPage />} />
+            <Route path=\"/portfolio/:id\" element={<ProjectDetailsPage />} />
+            <Route path=\"/events\" element={<EventsCalendarPage />} />
+            <Route path=\"/about\" element={<AboutPage />} />
+            <Route path=\"/contact\" element={<ContactPage />} />
+            <Route path=\"/insights\" element={<InsightsPage />} />
+            <Route path=\"/ai-design-studio\" element={<AIDesignStudioPage />} />
+            <Route path=\"/exhibition-studio\" element={<ExhibitionStudioPage />} />
+            <Route path=\"/design-result\" element={<DesignResultPage />} />
+            <Route path=\"/event-studio\" element={<EventStudioPage />} />
+            <Route path=\"/event-result\" element={<EventResultPage />} />
+            <Route path=\"/interior-studio\" element={<InteriorStudioPage />} />
+            <Route path=\"/interior-result\" element={<InteriorResultPage />} />
+            <Route path=\"/roi-calculator\" element={<ROICalculatorPage />} />
+            <Route path=\"/testimonials\" element={<TestimonialsPage />} />
+            {/* Lead Magnet Routes */}
+            <Route path=\"/resources/exhibition-guide\" element={<ExhibitionGuidePage />} />
+            <Route path=\"/resources/trends-report\" element={<TrendsReportPage />} />
+            <Route path=\"/resources/cost-calculator\" element={<CostCalculatorPage />} />
+            <Route path=\"/resources/cost-guide\" element={<CostGuidePage />} />
+            <Route path=\"/consultation\" element={<ConsultationPage />} />
+          </Routes>
+        </AnimatePresence>
+      </Layout>
+    </ApiKeyProvider>
+  );
 };
 
 export default App;
